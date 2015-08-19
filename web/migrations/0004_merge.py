b85f6f258229f54c5c43dea98ed4fce7960f25c4
Tiina Romu <tiina.romu@futurice.com>
2015-08-19 18:16:38 +0300

    Merge migrations

---
+++ web/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('web', '0003_auto_20150819_1453'),
+        ('web', '0002_auto_20150819_1407'),
+    ]
+
+    operations = [
+    ]
