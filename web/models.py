3fb90c1c84f03ce82fc7ed038f5ded5ffa530433
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2015-08-25 12:55:58 +0300

    Remove unused import

--- web/models.py
+++ web/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.conf import settings
 import googlemaps
-import json
 
 
 class UserManager(BaseUserManager):
