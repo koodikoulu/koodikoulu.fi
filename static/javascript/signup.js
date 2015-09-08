9a529f212ed0eabb4de72338784bda8ef6966060
Joonas Rouhiainen <joonas.rouhiainen@reaktor.com>
2015-09-08 16:55:20 +0300

    Enable signup form submit only when required fields are filled.

--- static/javascript/signup.js
+++ static/javascript/signup.js
@@ -1,36 +1,55 @@
-$(document).ready(function() {
+$(function() {
+  var $form = $('form#signup-form')
+  var $submits = $form.find('[type=submit]')
+  var $spinner = $form.find('.spinner')
 
-  function createSignUp(form, input, loader) {
-    $.ajax({
-      url: form.attr('action'),
-      type: form.attr('method'),
-      data: form.serialize(),
-      success: function(data) {
-        form[0].reset();
-        var message = form.find('p.submit-message');
-        message.text('Kiitos ilmoittautumisesta!');
-        message.css('color', 'green');
-        input.show();
-        loader.remove();
-      },
-      error: function(data) {
-        var message = form.find('p.submit-message');
-        message.text('Jotakin meni pieleen. Yritä uudelleen.');
-        message.css('color', 'red');
-        input.show();
-        loader.remove();
-      }
-    });
+  function formState($form) {
+    return R.mergeAll(
+      $form.serializeArray().map(function(o) {
+        var obj = {}
+        obj[o.name] = o.value
+        return obj
+    }))
   }
 
-  $('input[type="submit"]').on('click', function(event) {
-    event.preventDefault();
-    var form = $(this).closest('form');
-    var input = $(this);
-    var loader = $('<img src="/static/media/ajax-loader.gif" alt="loading" id="loader">');
-    input.after(loader);
-    input.hide();
-    createSignUp(form, input, loader);
-    return false;
-  });
-});
+  var requiredKeys = $form.find('label.required')
+    .map(function(i, el) {
+      return $(el).attr('for').split('_')[1]
+    }).get()
+
+  $form.find('input').keyup(function() {
+    var data = formState($form)
+
+    var formValid = requiredKeys.map(function(key) {
+      return !R.isEmpty(data[key])
+    }).reduce(function(a, b) { return a && b })
+
+    $submits.prop('disabled', !formValid)
+  })
+
+  $form.submit(function(e) {
+    e.preventDefault()
+    $form.find('.submit-message').hide()
+    $spinner.show()
+    $submits.prop('disabled', true)
+
+    $.ajax({
+      url: $form.attr('action'),
+      type: $form.attr('method'),
+      data: $form.serialize(),
+
+      success: function(res) {
+        $spinner.hide()
+        $submits.prop('disabled', false)
+        $form.find('.submit-message.success').show()
+      },
+
+      error: function(res) {
+        $spinner.hide()
+        $submits.prop('disabled', false)
+        $form.find('.submit-message.error').show()
+      }
+    })
+  })
+
+})
