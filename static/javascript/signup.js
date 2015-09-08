e65d91dfd7e2661a1a3579c7b7189f761b61d158
Joonas Rouhiainen <joonas.rouhiainen@reaktor.com>
2015-09-08 17:17:27 +0300

    Use class instead of id for signup-form, single page may contain multiple forms.

--- static/javascript/signup.js
+++ static/javascript/signup.js
@@ -1,56 +1,61 @@
 $(function() {
-  var $form = $('form#signup-form')
-  var $submits = $form.find('[type=submit]')
-  var $spinner = $form.find('.spinner')
-
-  function formState($form) {
-    return R.mergeAll(
-      $form.serializeArray().map(function(o) {
-        var obj = {}
-        obj[o.name] = o.value
-        return obj
-    }))
-  }
-
-  var requiredKeys = $form.find('label.required')
-    .map(function(i, el) {
-      return $(el).attr('for').split('_')[1]
-    }).get()
-
-  $form.find('input').keyup(function() {
-    var data = formState($form)
-
-    var formValid = requiredKeys.map(function(key) {
-      return !R.isEmpty(data[key])
-    }).reduce(function(a, b) { return a && b })
-
-    $submits.prop('disabled', !formValid)
+  $('form.signup-form').each(function(i, el) {
+    signupForm($(el))
   })
 
-  $form.submit(function(e) {
-    e.preventDefault()
-    $form.find('.submit-message').hide()
-    $spinner.show()
-    $submits.prop('disabled', true)
+  function signupForm($form) {
+    var $submits = $form.find('[type=submit]')
+    var $spinner = $form.find('.spinner')
 
-    $.ajax({
-      url: $form.attr('action'),
-      type: $form.attr('method'),
-      data: $form.serialize(),
+    function formState($form) {
+      return R.mergeAll(
+          $form.serializeArray().map(function(o) {
+            var obj = {}
+            obj[o.name] = o.value
+            return obj
+          }))
+    }
 
-      success: function(res) {
-        $spinner.hide()
-        $form.trigger('reset')
-        $submits.prop('disabled', false)
-        $form.find('.submit-message.success').show()
-      },
+    var requiredKeys = $form.find('label.required')
+        .map(function(i, el) {
+          return $(el).attr('for').split('_')[1]
+        }).get()
 
-      error: function(res) {
-        $spinner.hide()
-        $submits.prop('disabled', false)
-        $form.find('.submit-message.error').show()
-      }
+    $form.find('input').keyup(function() {
+      var data = formState($form)
+
+      var formValid = requiredKeys.map(function(key) {
+        return !R.isEmpty(data[key])
+      }).reduce(function(a, b) { return a && b })
+
+      $submits.prop('disabled', !formValid)
     })
-  })
+
+    $form.submit(function(e) {
+      e.preventDefault()
+      $form.find('.submit-message').hide()
+      $spinner.show()
+      $submits.prop('disabled', true)
+
+      $.ajax({
+        url: $form.attr('action'),
+        type: $form.attr('method'),
+        data: $form.serialize(),
+
+        success: function(res) {
+          $spinner.hide()
+          $form.trigger('reset')
+          $submits.prop('disabled', false)
+          $form.find('.submit-message.success').show()
+        },
+
+        error: function(res) {
+          $spinner.hide()
+          $submits.prop('disabled', false)
+          $form.find('.submit-message.error').show()
+        }
+      })
+    })
+  }
 
 })
