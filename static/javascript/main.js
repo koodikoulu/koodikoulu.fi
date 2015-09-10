a998a04484d21030dfae2bdde49c849e0dcf8ff0
Joonas Rouhiainen <joonas.rouhiainen@reaktor.com>
2015-09-10 16:47:35 +0300

    Scroll to content when toggling accordion panels.

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -2,11 +2,21 @@ $(function() {
   var $panels = $('.panel')
   var activeClass = 'active'
 
+  // Active class for togglable panels.
+
   $panels.on('show.bs.collapse', function() {
     $(this).addClass(activeClass)
   })
-
   $panels.on('hide.bs.collapse', function() {
     $(this).removeClass(activeClass)
   })
+
+  // Scroll window to content when toggling accordion panels.
+
+  $('#accordion').on('shown.bs.collapse', function (e) {
+    $('html,body').animate({
+      scrollTop: $($(e.target).find('.panel-body')).offset().top - 115
+    }, 100)
+  })
+
 })
