6c3988800f7f9fd86f8d9ff65c10f9335e713bfe
Tiina Romu <tiina.romu@futurice.com>
2015-09-14 16:29:00 +0300

    merge db migrations

---
+++ web/migrations/0012_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('web', '0011_auto_20150914_1041'),
+        ('web', '0011_event_decoded_location'),
+    ]
+
+    operations = [
+    ]
