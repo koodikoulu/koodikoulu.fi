2c8efc71ca81afaf52e9366ceccc799b7252a703
Joonas Rouhiainen <joonas.rouhiainen@reaktor.com>
2015-09-16 16:05:17 +0300

    Make participant removal delete the row elements again.

--- static/javascript/own-events.js
+++ static/javascript/own-events.js
@@ -1,36 +1,36 @@
 $(document).ready(function() {
 
   function csrfSafeMethod(method) {
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method))
   }
 
   function removeParticipant(id) {
     $.ajaxSetup({
       beforeSend: function(xhr, settings) {
         if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-          xhr.setRequestHeader('X-CSRFToken', Cookies.get('csrftoken'));
+          xhr.setRequestHeader('X-CSRFToken', Cookies.get('csrftoken'))
         }
       }
-    });
+    })
+    
     $.ajax({
       url: '/participant/' + id + '/',
       type: 'POST',
       success: function(data) {
-        var object = $('#participant-' + id);
-        object.remove();
+        $('.participant-' + id).remove()
       },
       error: function(data) {
-        alert('Jotakin meni pieleen: ' + data.statusText);
+        alert('Jotakin meni pieleen: ' + data.statusText)
       }
-    });
+    })
 
   }
 
   $('input[type="submit"]').on('click', function(event) {
-    var answer = confirm('Haluatko varmasti poistaa osallistujan?');
+    var answer = confirm('Haluatko varmasti poistaa osallistujan?')
     if (answer) {
-      removeParticipant($(this).attr('name'));
+      removeParticipant($(this).attr('name'))
     }
-  });
+  })
 
-});
+})
